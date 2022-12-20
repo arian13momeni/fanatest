a4904b73f7c0eeac82ed7fec139f24f197d86f8b
ariAN <114738229+arian13momeni@users.noreply.github.com>
2022-12-20 22:25:39 +0330

    Create stup.py

---
+++ stup.py
@@ -0,0 +1,14 @@
+from pyfiglet import
+from colorama import
+from time import
+print(Fore.BLUE, "")
+print(figlet_format("fana"))
+url = input("please enter url whit https : "):
+    print(Fore.GREEN, "")
+    print("check url http")
+    sleep(1)
+    print("good")
+    print("check security")
+    sleep(2)
+    print("low")
+    print("you can infiltrate the site")
